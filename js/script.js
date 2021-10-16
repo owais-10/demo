e8626fddc24912c59a88ebd120298d9504e06c89
Owais <mansuriowais5@gmail.com>
2021-10-16 11:40:01 +0530

    omnifood

--- js/script.js
+++ js/script.js
@@ -1,3 +1,104 @@
+console.log ("hello world")
+
+const myName = "Owais Mansuri"
+const h1 = document.querySelector(".heading-primary")
+console.log(myName)
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+// ////////////////////////////////////////////////////////////
+// MOBILE NAVIGATION WORKS
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function() {
+  headerEl.classList.toggle("nav-open");
+});
+
+
+
+// ////////////////////////////////////////////////////////////
+// SMOOTH SCROLLING ANIMATION
+
+const allLinks = document.querySelectorAll("a:link")
+
+allLinks.forEach(function(links) {
+  links.addEventListener("click", function(e){
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // scrollback to top
+    if (href === "#")
+     window.scrollTo ({
+       top: 0,
+       behavior: smooth ,
+     });
+
+    //  scroll toother links
+     if (href !== "#"  && href.startsWith("#")) {
+       const sectionEL = document.querySelector(href);
+       sectionEL.scrollIntoView( {behavior:"smooth"});
+     }
+
+    //  CLOSE MOBILE NAVIGATION
+    if(link.classList.conatin("main-nav-link"))
+     headerEl.classList.toggle("nav-open");
+  });
+});
+
+
+// ////////////////////////////////////////////////////////
+// sticky navigation
+
+const sectionHeroEl = document.querySelector("section-hero");
+
+const obs = new IntersectionObserver (
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px"
+  }
+);
+
+obs.observe(sectionHeroEl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
